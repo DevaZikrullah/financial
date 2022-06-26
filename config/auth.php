7d3d63e93f418ab50a580faf86734684b68b0397
DevaZikrullah <zdeva804@gmail.com>
2022-06-26 20:30:26 +0700

    add Login With JWTauth (not completed)

--- config/auth.php
+++ config/auth.php
@@ -14,10 +14,16 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api',
         'passwords' => 'users',
     ],
 
+    'guards' => [
+        'api' => [
+            'driver' => 'jwt',
+            'provider' => 'users',
+        ],
+    ],
     /*
     |--------------------------------------------------------------------------
     | Authentication Guards
